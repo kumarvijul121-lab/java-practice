0d6d118a0df26dcbf5dc5300d880bcb0e9818087
Vijul | BBA Student | Aspiring Web Developer <kumarvijul121@gmail.com>
2026-03-12 12:11:06 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 720300
+let accountEmail = "vscode3432@gmail.com"
+var accountPassword = "822828"
+accountCity = "Karnal"
+
+
+
+// accountId = 8 // not allowed
+
+accountEmail = "Mynewcodechange@gmail.com"
+accountPassword = "723343"
+accountCity = "Kurukhshetra"
+
+
+console.log(accountId);
+
+ console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
