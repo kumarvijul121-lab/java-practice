8fe044d11618a1ed00ac83431fbcbd84e6c64677
Vijul | BBA Student | Aspiring Web Developer <kumarvijul121@gmail.com>
2026-03-14 11:01:49 +0000

    console config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,6 +2,7 @@ const accountId = 720300
 let accountEmail = "vscode3432@gmail.com"
 var accountPassword = "822828"
 accountCity = "Karnal"
+accountState = "Haryana"
 
 
 
@@ -10,8 +11,12 @@ accountCity = "Karnal"
 accountEmail = "Mynewcodechange@gmail.com"
 accountPassword = "723343"
 accountCity = "Kurukhshetra"
+accountState = "Punjab"
 
 
 console.log(accountId);
+ 
+// prefer not to use var
+//  because of issue in block scope and functional scope
 
  console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // Treats all JS code as newer version and throws error for unsafe actions
+
+alert("Hello World") // we are using nodejs so alert will not work here , only works in browser
+
+.console.log(3 
+      +
+       3) //code readability should be high
+
+    console.log (" Viul")
+
+    let name = "Vijul"
+    let = age = 19
+   let isloggedIn = true 
+
+
+   // number => 1, 3.14, -5, etc
+   // bigint => 9007199254740991n (for very large integers)
+   // boolean => true/false
+   // null => standalone value that represents "nothing"
+   // undefined => variable that is not assigned a value yet
+   // symbol => unique identifier for obects (use Symbol() to create)
+
+
+
+
+   //Object => collection of key value pairs
+
+   console.log(typeof "Vijul")
+
