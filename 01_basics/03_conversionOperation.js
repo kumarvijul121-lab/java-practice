9f2a1d11bd227682959a049bdb80e00486fd4950
Vijul | BBA Student | Aspiring Web Developer <kumarvijul121@gmail.com>
2026-03-15 08:23:10 +0000

    Operation discussed

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,56 @@
+// let age = 19
+// let score = 90 
+// let score = "90" // string
+// let score = "90abc"
+// let score = null
+
+// let score = undefined 
+//Nan => Not a Number
+
+// let score = true
+//  // 1 => true , 0 => false
+
+let score = "Vijul"
+
+
+
+
+//  console.log(typeof score);
+//  console.log(typeof (score));
+
+// let valueInNumber = Number(score) 
+
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber); 
+
+
+
+
+// NaN => Not a Number
+// "90" => 90
+// "90abc" => NaN
+// null =>0
+// undefined => NaN
+// true => 1
+// false => 0
+
+
+
+
+// let is LoggedIn = true //boolean
+
+
+
+
+let isLoggedIn =  "Vijul" //true //"" false   //9 true; 
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+
+let someNumber = 100
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
