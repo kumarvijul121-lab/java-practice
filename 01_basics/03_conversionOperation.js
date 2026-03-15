71cbc739750432209e2f12af23485aab948a66f9
Vijul | BBA Student | Aspiring Web Developer <kumarvijul121@gmail.com>
2026-03-15 12:55:09 +0000

    Operation discussed

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -4,13 +4,13 @@
 // let score = "90abc"
 // let score = null
 
-// let score = undefined 
-//Nan => Not a Number
+//  let score = undefined 
+//NaN => Not a Number
 
-// let score = true
-//  // 1 => true , 0 => false
+//  let score = true
+//  let score = 1 //=> true , 0 => false
 
-let score = "Vijul"
+ let score = "Vijul"
 
 
 
@@ -43,14 +43,54 @@ let score = "Vijul"
 
 
 
-let isLoggedIn =  "Vijul" //true //"" false   //9 true; 
+ let isLoggedIn =  "Vijul" //true //"" false   //9 true; 
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 
 
-let someNumber = 100
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+ let someNumber = 100
+ let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+//*************************** Operations ********************************
+
+let value = 3 
+let negValue = -value
+// console.log(negValue);
+
+// console.log(3 + 4);//output:7
+// console.log(3 - 4);//output:-1
+// console.log(3 * 4);//output:12
+// console.log(3 / 4);//output:0.75
+// console.log(3 % 4);//output:3
+// console.log(3 ** 4);//output:81
+
+let str1 = "Hello"
+let str2 = " My World"
+let str3 = str1 + str2 
+
+// console.log(str3); //output: Hello My World
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true)
+// console.log(+"")
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+ let gameCounter = 100
+//  gameCounter++;
+ ++gameCounter;
+ console.log(gameCounter);
+
+ // link to study
+ //https://tc39.es/ecma262/2023/multipage/abstract-operations.html
+ 
