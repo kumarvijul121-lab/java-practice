3011b0e327fb4ef06235d02740fbc398f7a3c2b6
Vijul | BBA Student | Aspiring Web Developer <kumarvijul121@gmail.com>
2026-03-16 07:49:37 +0000

    comparison of conversion

---
+++ 01_basics/conversion.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 != 1);
+// console.log(2 == 1);
+
+// console.log("2" > 1);
+// console.log(2 > "1");
+// console.log(2 > "Vijul");
+// console.log(2 == "2");
+
+
+ console.log(undefined == 0);
+ console.log(undefined > 0);
+ console.log(undefined < 0);
+
+
+// ===> strict equality operator => it checks value + datatype
+// ==> loose equity operator => it checks only value not datatype
+
+console.log("2" === 2);
+console.log(null === 0);
+
+
