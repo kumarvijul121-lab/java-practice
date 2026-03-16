91b5bef70eb3e22aee48e6c55ba431d1cf5bdcd4
Vijul | BBA Student | Aspiring Web Developer <kumarvijul121@gmail.com>
2026-03-16 07:51:41 +0000

    comparision of conversion

--- 01_basics/conversion.js
+++ 01_basics/conversion.js
@@ -1,13 +1,15 @@
-// console.log(2 > 1);
-// console.log(2 < 1);
-// console.log(2 >= 1);
-// console.log(2 != 1);
-// console.log(2 == 1);
+ console.log(2 > 1);
+ console.log(2 < 1);
+ console.log(2 >= 1);
+ console.log(2 != 1);
+ console.log(2 == 1);
 
-// console.log("2" > 1);
-// console.log(2 > "1");
-// console.log(2 > "Vijul");
-// console.log(2 == "2");
+
+
+ console.log("2" > 1);
+ console.log(2 > "1");
+ console.log(2 > "Vijul");
+ console.log(2 == "2");
 
 
  console.log(undefined == 0);
