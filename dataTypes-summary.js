dc1628ff586e41eec0aa3e9abe3eba77106168c3
Vijul | BBA Student | Aspiring Web Developer <kumarvijul121@gmail.com>
2026-03-20 08:22:28 +0000

    change in location of file

--- dataTypes-summary.js
+++ dataTypes-summary.js
@@ -6,46 +6,46 @@
 
 
 // String
- let name = "John Doe";
- console.log(typeof name); // Output String
+  let name = "John Doe";
+  console.log(typeof name); // Output String
 
 
 //Number
- const score = 30;
- const scoreValue = 30.5;
- console.log(typeof score);// Output Number
- console.log(typeof scoreValue); // Output Number
+  const score = 30;
+  const scoreValue = 30.5;
+  console.log(typeof score);// Output Number
+  console.log(typeof scoreValue); // Output Number
 
 
 // Boolean
- let isStudent = true;
- const LoggedIn = false;
- console.log(typeof isStudent); //Output: boolean
-console.log(typeof LoggedIn); //Output: boolean
+  let isStudent = true;
+  const LoggedIn = false;
+  console.log(typeof isStudent); //Output: boolean
+// console.log(typeof LoggedIn); //Output: boolean
 
 
 // Null 
- let emptyValue = null;
- const outsideTemp = null;
- console.log(typeof emptyValue); // Output: object
- console.log(typeof outsideTemp); // Output: object
+  let emptyValue = null;
+  const outsideTemp = null;
+  console.log(typeof emptyValue); // Output: object
+  console.log(typeof outsideTemp); // Output: object
 
 // Undefined
- let age;
- let userEmail;
- console.log(typeof age); // Output; undefined
- console.log(typeof userEmail); // Output; undefined
+  let age;
+  let userEmail;
+  console.log(typeof age); // Output; undefined
+  console.log(typeof userEmail); // Output; undefined
 
 //Symbol
-const uniqueId = Symbol("24765702");
- const id = Symbol("12345789");
-  console.log(typeof uniqueId); // Output; symbol
-   console.log(uniqueId === id); // Output;
+ const uniqueId = Symbol("24765702");
+  const id = Symbol("12345789");
+   console.log(typeof uniqueId); // Output; symbol
+    console.log(uniqueId === id); // Output;
 // false
 
  // BigInt
- const bigNumber = 23748234857893456345n;
- console.log(typeof bigNumber); // Output: bigint
+const bigNumber = 23748234857893456345n;
+console.log(typeof bigNumber); // Output: bigint
 
 
 
@@ -56,19 +56,44 @@ const uniqueId = Symbol("24765702");
  // object, array, function 
 
 
- const heros =["Netro", "Naagraj", "Doga"];
- let myObj = {
-    name : "Viul",
-    age : 25,
- }
+  const heros =["Netro", "Naagraj", "Doga"];
+  let myObj = {
+     name : "Viul",
+     age : 25,
+  }
 
- const myFunction = function(){
+  const myFunction = function(){
   console.log("Hello World");
- }
+  }
 
- console.log(typeof myFunction);
- console.log(typeof heros);
+  console.log(typeof myFunction);
+  console.log(typeof heros);
  
  // https://262.ecma-international.org/5.1/#sec-11.4.3
 
 
+
+ //+++++++++++++++++++++++++++++++++++++++++++++++++++++
+  
+
+ // Stack(Primitive  (Number, String, Boolean, Null, Undefined, Symbol, BigInt) always Show Copy), Heap always show refrence (originnal value)(Non-primitive)
+
+ let myYoutubeChannel = "CodeWithVijul"
+ let anotherChannel =
+ "myyoutubechannel";
+
+ console.log(myYoutubeChannel);
+ console.log(anotherChannel);
+
+let userone = {
+      email : "vijul@example.com",
+      upi : "vijul@okaxis"
+
+}
+
+ let userTwo = userone;
+
+ userTwo.email = "kumarec@examp.com";
+
+console.log(userone.email);
+console.log(userTwo.email);
