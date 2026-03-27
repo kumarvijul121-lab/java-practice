3f0192c1ff5cb2de28a5d11fe7cd0d9f0eb83249
Vijul | BBA Student | Aspiring Web Developer <kumarvijul121@gmail.com>
2026-03-27 11:10:10 +0000

    Operation discussed

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,30 +1,24 @@
-// let age = 19
-// let score = 90 
-// let score = "90" // string
-// let score = "90abc"
-// let score = null
-
-//  let score = undefined 
+let age = 19
+let score = 90 
+let score = "90" // string
+let score = "90abc"
+let score = null
+let score = undefined 
 //NaN => Not a Number
 
-//  let score = true
+let score = true
 //  let score = 1 //=> true , 0 => false
 
- let score = "Vijul"
+let score = "Vijul"
+
+console.log(typeof score);
+console.log(typeof (score));
 
 
+let valueInNumber = Number(score) 
 
-
-//  console.log(typeof score);
-//  console.log(typeof (score));
-
-// let valueInNumber = Number(score) 
-
-// console.log(typeof valueInNumber);
-
-// console.log(valueInNumber); 
-
-
+console.log(typeof valueInNumber);
+console.log(valueInNumber); 
 
 
 // NaN => Not a Number
@@ -35,25 +29,20 @@
 // true => 1
 // false => 0
 
-
-
-
 // let is LoggedIn = true //boolean
 
 
+let isLoggedIn =  "Vijul" //true //"" false   //9 true; 
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
 
 
- let isLoggedIn =  "Vijul" //true //"" false   //9 true; 
+let someNumber = 100
+let stringNumber = String(someNumber)
 
- let booleanIsLoggedIn = Boolean(isLoggedIn)
-// console.log(booleanIsLoggedIn);
-
-
-
- let someNumber = 100
- let stringNumber = String(someNumber)
-// console.log(stringNumber);
-// console.log(typeof stringNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
 
 
 
@@ -61,28 +50,29 @@
 
 let value = 3 
 let negValue = -value
-// console.log(negValue);
 
-// console.log(3 + 4);//output:7
-// console.log(3 - 4);//output:-1
-// console.log(3 * 4);//output:12
-// console.log(3 / 4);//output:0.75
-// console.log(3 % 4);//output:3
-// console.log(3 ** 4);//output:81
+console.log(negValue);
+console.log(3 + 4);//output:7
+console.log(3 - 4);//output:-1
+console.log(3 * 4);//output:12
+console.log(3 / 4);//output:0.75
+console.log(3 % 4);//output:3
+console.log(3 ** 4);//output:81
+
 
 let str1 = "Hello"
 let str2 = " My World"
 let str3 = str1 + str2 
 
-// console.log(str3); //output: Hello My World
+console.log(str3); //output: Hello My World
 
-// console.log(1 + "2");
-// console.log("1" + 2);
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+console.log(1 + "2");
+console.log("1" + 2);
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
 
-// console.log(+true)
-// console.log(+"")
+console.log(+true)
+console.log(+"")
 
 let num1, num2, num3
 num1 = num2 = num3 = 2 + 2
