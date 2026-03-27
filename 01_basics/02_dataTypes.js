cd6ff346a10bbd4a27dd118a5f05210850b1eddc
Vijul | BBA Student | Aspiring Web Developer <kumarvijul121@gmail.com>
2026-03-27 11:11:17 +0000

    dataTypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,31 +1,25 @@
 "use strict"; // Treats all JS code as newer version and throws error for unsafe actions
 
 //alert("Hello World") // we are using nodejs so alert will not work here , only works in browser
+console.log(3  
+     + 3) //code readability should be high
 
- console.log(3  
-      +
-       3) //code readability should be high
+console.log ("Vijul")
 
-    console.log ("Vijul")
+let name = "Vijul"
+let age = 19
+let isloggedIn = false
+let state;
 
-    let name = "Vijul"
-    let age = 19
-   let isloggedIn = false
-   let state;
+// number => 1, 3.14, -5, etc
+// bigint => 9007199254740991n (for very large integers)
+// boolean => true/false
+// null => standalone value that represents "nothing"
+// undefined => variable that is not assigned a value yet
+// symbol => unique identifier for obects (use Symbol() to create)
+//Object => collection of key value pairs
 
-
-   // number => 1, 3.14, -5, etc
-   // bigint => 9007199254740991n (for very large integers)
-   // boolean => true/false
-   // null => standalone value that represents "nothing"
-   // undefined => variable that is not assigned a value yet
-   // symbol => unique identifier for obects (use Symbol() to create)
-
-
-
-   //Object => collection of key value pairs
-
-   console.log(typeof undefined) //undefiened
-   console.log (typeof null); //object
+console.log(typeof undefined) //undefiened
+console.log (typeof null); //object
    
 
