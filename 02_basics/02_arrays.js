371320ee269828626c2f5f6e4d9a86b5ee9b1246
Vijul | BBA Student | Aspiring Web Developer <kumarvijul121@gmail.com>
2026-03-27 11:12:15 +0000

    Array part 1

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,12 @@
+const marvel_heroes = ["ironman", "spiderman", "thor"]
+const dc_heroes = ["superman", "flash", "batman"]
+
+//  marvel_heroes.push(dc_heroes)
+
+//  console.log(marvel_heroes);
+//  console.log(marvel_heroes[3][1]);
+
+const all_heroes = marvel_heroes.concat(dc_heroes)
+console.log(all_heroes);
+
+ 
