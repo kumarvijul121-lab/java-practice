35fedb07699057e05aba534e433f5ae58db50cd3
Vijul | BBA Student | Aspiring Web Developer <kumarvijul121@gmail.com>
2026-04-01 15:26:26 +0000

    json talk

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,67 @@
+const tinderUser = new Object()
+const tinderuser = {}
+
+// console.log(tinderUser);
+// console.log(tinderuser);
+
+
+tinderUser.id = "abc123"
+tinderUser.name = "sammy"
+tinderUser.isloggedIn = false
+
+
+const regularUser = {
+    email:  "someemail@gamil.com",
+    fullname: {
+        userfullname: {
+            firstname: "Vijul",
+            lastname: "Kumar"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+
+// const obj3 = {obj1, obj2}
+//const obj3 = Object.assign({}, obj1, obj2, obj4) // minimum use
+
+const obj3 = {...obj1, ...obj2, ...obj4}
+console.log(obj3);
+
+ //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+
+
+
+// when values comes in data Base
+
+const users = [
+
+        {
+            id: 2,
+            email: "raume@gmail.com"
+        },
+        {
+            id: 2,
+            email: "raume@gmail.com"
+        },
+        {
+            id: 2,
+            email: "raume@gmail.com"
+        },
+
+    
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLogged'));
