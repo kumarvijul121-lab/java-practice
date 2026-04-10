ae1511bdf141c57a212f3e036397f97cf174257e
Vijul | BBA Student | Aspiring Web Developer <kumarvijul121@gmail.com>
2026-04-10 16:14:00 +0000

    taking parameters in functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -11,15 +11,15 @@ function SayMyName(){
 
 
 function addTwoNumbers(number1, number2){
-    //let result = number1 + number2
-    // console.log("Vijul Kumar");
-    // return result
+    let result = number1 + number2
+     console.log("Vijul Kumar");
+     return result
     return number1 + number2
     
     
 }
-// const result = addTwoNumbers(3, 5)
-// console.log("Result: ", result); 
+ const result = addTwoNumbers(3, 5)
+ console.log("Result: ", result); 
 
 
 function loginUserMessage(username = "Sam"){
@@ -33,5 +33,40 @@ function loginUserMessage(username = "Sam"){
 }
 
 
-// console.log(loginUserMessage("Umang"))
-console.log(loginUserMessage())
+  console.log(loginUserMessage("Umang"))
+ console.log(loginUserMessage())
+
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1 
+}
+
+
+ console.log(calculateCartPrice(200, 400, 100, 2000));
+
+const user = {
+    username: "Vijul",
+    price: 200
+};
+
+function handleObject(anyobeject){
+     console.log(`Username is ${anyobeject.username} and price is ${anyobeject.price}`);
+    
+}
+
+// handleObject(user)
+handleObject({
+    username:"sam",
+    price: 300
+})
+
+
+//Array ke liye function
+const myNewArray = [100, 220, 300, 400,]
+function returnSecondValue(getarray){
+    return getarray [1]
+}
+ console.log(returnSecondValue(myNewArray));
+
+console.log(returnSecondValue([100, 220, 300, 1000, 5000]));
