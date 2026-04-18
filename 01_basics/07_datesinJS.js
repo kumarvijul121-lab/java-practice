4380b0169554aae318d0cb2a8d85f9cc9dfafe1d
Vijul | BBA Student | Aspiring Web Developer <kumarvijul121@gmail.com>
2026-04-18 10:45:19 +0000

    date and time

---
+++ 01_basics/07_datesinJS.js
@@ -0,0 +1,17 @@
+// Dates
+
+const myDate = new Date();
+console.log(myDate);
+
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.getFullYear());
+// console.log(myDate.getMonth());
+// console.log(myDate.getDate());
+// console.log(myDate.getTime());
+// console.log(typeof myDate);
+
+let myCreateDate = new Date("2026-10-15");
+console.log(myCreateDate.toDateString);
