8826dc3f287483d43691308265fdac591d38f184
Vijul | BBA Student | Aspiring Web Developer <kumarvijul121@gmail.com>
2026-04-18 10:44:20 +0000

    scope this and arrow

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,16 +1,57 @@
 // var c = 300
 let a = 300
-
 if (true) {
-let a = 10
-const b = 20
-console.log("INNER:", a);
+    let a = 10
+    const b = 20
+    // console.log("INNER:", a);
 function addnum(){
+
+}
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one (){
+    const userName = "Vijul"
+
+    function two(){
+        const website = "youtube"
+        // console.log(userName);
+    }
+    // console.log(website);
+
+    two()
     
 }
+
+one()
+
+if (true) {
+    const userName = "Vijul"
+    if (userName === "Vijul") {
+        const website = "youtube"
+        // console.log(userName + website);
+    }
+    // console.log(website);
+    
+
 }
+// console.log(userName);
 
-console.log(a);
-console.log(b);
-console.log(c);
 
+// ++++++++++++++++++ interesting ++++++++++++++++++++++++
+
+console.log(addone(5));
+ 
+function addone(num){
+    return num + 1
+}
+// addone(5)
+
+// addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
+addone(5)
